661771c59a28c7b6479ac8465f687aac0f0bf549
Gerben <Gerben@145.18.167.163>
2014-02-03 17:09:48 +0100

    added mustard-experiments project

---
+++ mustard-experiments/src/main/java/org/data2semantics/mustard/experiments/Experiment.java
@@ -0,0 +1,17 @@
+package org.data2semantics.mustard.experiments;
+
+public class Experiment {
+
+	public Experiment() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
