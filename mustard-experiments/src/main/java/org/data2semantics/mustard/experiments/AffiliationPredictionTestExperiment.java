804b9f08266de69dbe6c7c410236f28124ee194a
Gerben <Gerben@VeryZenBook>
2014-08-20 17:36:42 +0200

    updates

--- mustard-experiments/src/main/java/org/data2semantics/mustard/experiments/AffiliationPredictionTestExperiment.java
+++ mustard-experiments/src/main/java/org/data2semantics/mustard/experiments/AffiliationPredictionTestExperiment.java
@@ -73,11 +73,11 @@ public class AffiliationPredictionTestExperiment {
 		// 0.0001,0.001,0.01,0.1,
 		double[] cs = {1, 10, 100, 1000, 10000};	
 		long[] seeds = {11,21,31,41,51,61,71,81,91,101};
-		int[] iterations  = {0,1,2,3,4,5,6};
-		int[] pathDepths = {0,1,2,3,4};
+		int[] iterations  = {4};
+		int[] pathDepths = {4};
 		int[] depths = {1,2,3};
-		boolean[] inferencing = {false, true};
-		boolean[] reversal = {false, true};
+		boolean[] inferencing = {false};
+		boolean[] reversal = {false};
 
 
 		LibSVMParameters svmParms = new LibSVMParameters(LibSVMParameters.C_SVC, cs);
