71967ece5e1809341396930149ad9c7f7798bf0b
Gerben <Gerben@145.18.164.139>
2014-12-05 11:31:59 +0100

    changed MapLabel implementation to use array instead of HashMap

--- mustard/src/main/java/org/data2semantics/mustard/weisfeilerlehman/MapLabel.java
+++ mustard/src/main/java/org/data2semantics/mustard/weisfeilerlehman/MapLabel.java
@@ -1,6 +1,7 @@
 package org.data2semantics.mustard.weisfeilerlehman;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
@@ -11,53 +12,64 @@ import java.util.Set;
  *
  */
 public class MapLabel {
-	private Map<Integer,StringBuilder> map;
+	private StringBuilder[] map;
 	private Map<Integer, String> prevNBH;
 	private Map<Integer, Boolean> sameAsPrev;
-	
-	public MapLabel() {
-		map = new HashMap<Integer,StringBuilder>();
+	private Set<Integer> keySet;
+
+
+	public MapLabel(int mapSize) {
+		map = new StringBuilder[mapSize];
 		prevNBH = new HashMap<Integer, String>();
 		sameAsPrev = new HashMap<Integer, Boolean>();
+		keySet = new HashSet<Integer>();
 	}
-	
+
+
+	public MapLabel() {
+		this(3);
+	}
+
 	public Set<Integer> keySet() {
-		return map.keySet();
+		return keySet;
 	}
-	
+
 	public void put(Integer key, StringBuilder value) {
-		map.put(key, value);
+		if (map[key] == null) {
+			keySet.add(key);
+		}
+		map[key] = value;
 	}
-	
+
 	public StringBuilder get(Integer key) {
-		return map.get(key);
+		return map[key];
 	}
 
 	public boolean containsKey(Integer key) {
-		return map.containsKey(key);
+		return keySet.contains(key);
 	}
-	
+
 	public void clear(Integer key) {
-		map.get(key).delete(0, map.get(key).length());
+		map[key].delete(0, map[key].length());
 	}
 
 	@Override
 	public String toString() {
 		return map.toString();
 	}
-	
+
 	public void putPrevNBH(Integer key, String value) {
 		prevNBH.put(key, value);
 	}
-	
+
 	public String getPrevNBH(Integer key) {
 		return prevNBH.get(key);
 	}
-	
+
 	public void putSameAsPrev(Integer key, Boolean value) {
 		sameAsPrev.put(key, value);
 	}
-	
+
 	public boolean getSameAsPrev(Integer key) {
 		Boolean ret = sameAsPrev.get(key);
 		return (ret == null) ? false : ret;
