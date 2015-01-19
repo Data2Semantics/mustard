e03c303bc074b0412555f540edf19564070925a7
Jasper E.G. Oosterman <jasper.oosterman@gmail.com>
2015-01-19 19:34:45 +0100

    Outputs the number of non-zero features

--- mustard-experiments/src/main/java/org/data2semantics/mustard/experiments/utils/FeatureInspectionExperiment.java
+++ mustard-experiments/src/main/java/org/data2semantics/mustard/experiments/utils/FeatureInspectionExperiment.java
@@ -12,22 +12,20 @@ import org.data2semantics.mustard.kernels.FeatureInspector;
 import org.data2semantics.mustard.kernels.Kernel;
 import org.data2semantics.mustard.kernels.data.GraphData;
 import org.data2semantics.mustard.kernels.graphkernels.FeatureVectorKernel;
-import org.data2semantics.mustard.learners.Prediction;
 import org.data2semantics.mustard.learners.SparseVector;
-import org.data2semantics.mustard.learners.evaluation.EvaluationFunction;
 import org.data2semantics.mustard.learners.liblinear.LibLINEAR;
 import org.data2semantics.mustard.learners.liblinear.LibLINEARModel;
 import org.data2semantics.mustard.learners.liblinear.LibLINEARParameters;
 
-
-public class FeatureInspectionExperiment<D extends GraphData, K extends FeatureVectorKernel<D> & FeatureInspector>  {
+public class FeatureInspectionExperiment<D extends GraphData, K extends FeatureVectorKernel<D> & FeatureInspector> {
 	private D data;
 	private List<Double> labels;
 	private LibLINEARParameters svmParms;
 	private List<K> kernels;
 	private int maxFeatures;
 
-	public FeatureInspectionExperiment(List<K> kernels, D data,	List<Double> labels, LibLINEARParameters svmParms, int maxFeatures) {
+	public FeatureInspectionExperiment(List<K> kernels, D data, List<Double> labels, LibLINEARParameters svmParms,
+			int maxFeatures) {
 		this.data = data;
 		this.labels = labels;
 		this.svmParms = svmParms;
@@ -41,7 +39,7 @@ public class FeatureInspectionExperiment<D extends GraphData, K extends FeatureV
 
 		Map<Kernel, SparseVector[]> fvs = new HashMap<Kernel, SparseVector[]>();
 
-		System.out.println("Computing FVs...");	
+		System.out.println("Computing FVs...");
 		for (FeatureVectorKernel<D> kernel : kernels) {
 			SparseVector[] fv = kernel.computeFeatureVectors(data);
 			fvs.put(kernel, fv);
@@ -51,29 +49,37 @@ public class FeatureInspectionExperiment<D extends GraphData, K extends FeatureV
 			Collections.shuffle(tempFV, new Random(11));
 			fvs.put(k, tempFV.toArray(new SparseVector[0]));
 		}
-		Collections.shuffle(tempLabels, new Random(11));		
+		Collections.shuffle(tempLabels, new Random(11));
 
 		double[] target = new double[tempLabels.size()];
 		for (int i = 0; i < target.length; i++) {
 			target[i] = tempLabels.get(i);
 		}
 
-		System.out.println("Computing Model...");	
-		LibLINEARModel model = LibLINEAR.trainLinearModelWithMultipleFeatureVectors(fvs, target, svmParms);		
+		System.out.println("Computing Model...");
+		LibLINEARModel model = LibLINEAR.trainLinearModelWithMultipleFeatureVectors(fvs, target, svmParms);
 		LibLINEARModel.WeightIndexPair[][] fws = model.getFeatureWeights();
 		int[] intLabs = model.getLabels();
- 
+
 		// Sort them
 		for (int i = 0; i < fws.length; i++) {
 			FeatureInspector fi = (FeatureInspector) model.getKernelSetting();
 			Arrays.sort(fws[i]);
 			List<Integer> indices = new ArrayList<Integer>();
 
-			System.out.println("Class " + intLabs[i]);
+			//count the total number of features that are not zero
+			int nonZeroFeatures = 0;
+			for (int j = 0; j < fws[i].length; j++) {
+				if (fws[i][j].getWeight() > 0) {
+					nonZeroFeatures++;
+				}
+			}
+
+			System.out.println("Class " + intLabs[i] + " (" + nonZeroFeatures + " features ) ");
 			System.out.print("Index: Weight - ");
 
 			for (int k = 0; k < maxFeatures; k++) {
-				if (fws[i][k].getWeight() > 0){
+				if (fws[i][k].getWeight() > 0) {
 					indices.add(fws[i][k].getIndex());
 					System.out.print(fws[i][k].getIndex() + ": " + fws[i][k].getWeight() + ", ");
 				} else {
@@ -82,6 +88,6 @@ public class FeatureInspectionExperiment<D extends GraphData, K extends FeatureV
 			}
 			System.out.println("");
 			System.out.println("Labels        - " + fi.getFeatureDescriptions(indices));
-		}	
+		}
 	}
 }
