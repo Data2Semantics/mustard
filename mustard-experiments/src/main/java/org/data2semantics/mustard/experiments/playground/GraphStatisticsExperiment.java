0dfc438323b05f9a3ab308adcf2c891b5a7509df
Gerben <Gerben@145.18.166.192>
2015-06-12 14:37:49 +0200

    updated graph statistics exp

--- mustard-experiments/src/main/java/org/data2semantics/mustard/experiments/playground/GraphStatisticsExperiment.java
+++ mustard-experiments/src/main/java/org/data2semantics/mustard/experiments/playground/GraphStatisticsExperiment.java
@@ -1,5 +1,6 @@
 package org.data2semantics.mustard.experiments.playground;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -11,6 +12,7 @@ import org.data2semantics.mustard.experiments.data.BGSDataSet;
 import org.data2semantics.mustard.experiments.data.BGSLithoDataSet;
 import org.data2semantics.mustard.experiments.data.ClassificationDataSet;
 import org.data2semantics.mustard.experiments.data.LargeClassificationDataSet;
+import org.data2semantics.mustard.experiments.data.SubsetDataSet;
 import org.data2semantics.mustard.kernels.data.GraphList;
 import org.data2semantics.mustard.kernels.data.SingleDTGraph;
 import org.data2semantics.mustard.rdf.RDFDataSet;
@@ -24,9 +26,7 @@ import org.openrdf.rio.RDFFormat;
 
 public class GraphStatisticsExperiment {
 	private static String AIFB_FILE = "datasets/aifb-fixed_complete.n3";
-	private static String BGS_FOLDER =  "C:\\Users\\Gerben\\Dropbox\\data_bgs_ac_uk_ALL";
-	private static String AM_FOLDER =  "C:\\Users\\Gerben\\Dropbox\\AM_data";
-	private static String ISWC_FOLDER = "datasets/";
+	private static String BGS_FOLDER =  "C:\\Users\\Gerben\\onedrive\\d2s\\data_bgs_ac_uk_ALL";
 
 
 
@@ -35,32 +35,30 @@ public class GraphStatisticsExperiment {
 	 */
 	public static void main(String[] args) {
 
-		RDFDataSet tripleStore = new RDFFileDataSet(AIFB_FILE, RDFFormat.N3);
-		ClassificationDataSet ds = new AIFBDataSet(tripleStore);
-	
+		//RDFDataSet tripleStore = new RDFFileDataSet(AIFB_FILE, RDFFormat.N3);
+		//ClassificationDataSet ds = new AIFBDataSet(tripleStore);
+
 		//RDFDataSet tripleStore = new RDFFileDataSet(BGS_FOLDER, RDFFormat.NTRIPLES);
 		//ClassificationDataSet ds = new BGSLithoDataSet(tripleStore);
 		
-		//RDFDataSet tripleStore = new RDFFileDataSet(AM_FOLDER, RDFFormat.TURTLE);
-		//LargeClassificationDataSet ds = new AMDataSet(tripleStore, 10, 0.005, 5, 4, true);
+		//RDFDataSet tripleStore = new RDFFileDataSet("datasets/carcinogenesis.owl", RDFFormat.forFileName("datasets/carcinogenesis.owl"));
+		//ClassificationDataSet ds = new BGSLithoDataSet(tripleStore);
 
-		//RDFDataSet tripleStore = new RDFFileDataSet(BGS_FOLDER, RDFFormat.NTRIPLES);
-		//LargeClassificationDataSet ds = new BGSDataSet(tripleStore, "http://data.bgs.ac.uk/ref/Lexicon/hasTheme", 10, 0.05, 5, 3);
-
-		ds.create();
+		//ds.create();
 
 
 
-		boolean[] inference = {false, true};
-		int[] depths = {1,2,3};
+		//boolean[] inference = {false, true};
+		//int[] depths = {1};
 
 
+		/*
 		for (boolean inf : inference) {
 			for (int depth : depths) {
 
 				Set<Statement> st = RDFUtils.getStatements4Depth(tripleStore, ds.getRDFData().getInstances(), depth, inf);
 				st.removeAll(ds.getRDFData().getBlackList());
-				SingleDTGraph graph = RDFUtils.statements2Graph(st, RDFUtils.REGULAR_SPLIT_LITERALS, ds.getRDFData().getInstances(), true);
+				SingleDTGraph graph = RDFUtils.statements2Graph(st, RDFUtils.REGULAR_LITERALS, ds.getRDFData().getInstances(), true);
 
 
 				GraphList<DTGraph<String,String>> graphs = RDFUtils.getSubGraphs(graph.getGraph(), graph.getInstances(), depth);
@@ -71,8 +69,49 @@ public class GraphStatisticsExperiment {
 			}
 
 		}
+		*/
+		
+		int[] depths = {1};
 
+		String[] subsets = {"datasets/AMsubset1false", "datasets/AMsubset2false", "datasets/AMsubset3false", "datasets/AMsubset4false", 
+				"datasets/AMsubset5false", "datasets/AMsubset6false", "datasets/AMsubset7false",
+				"datasets/AMsubset8false", "datasets/AMsubset9false", "datasets/AMsubset10false"};
+		
+		
+		/*
+		String[] subsets = {"datasets/AMsubset1true", "datasets/AMsubset2true", "datasets/AMsubset3true", "datasets/AMsubset4true", 
+				"datasets/AMsubset5true", "datasets/AMsubset6true", "datasets/AMsubset7true",
+				"datasets/AMsubset8true", "datasets/AMsubset9true", "datasets/AMsubset10true"};
+		
+		String[] subsets = {"datasets/BGSsubset1false", "datasets/BGSsubset2false", "datasets/BGSsubset3false", "datasets/BGSsubset4false", 
+				"datasets/BGSsubset5false", "datasets/BGSsubset6false", "datasets/BGSsubset7false",
+				"datasets/BGSsubset8false", "datasets/BGSsubset9false", "datasets/BGSsubset10false"};
+		
+		String[] subsets = {"datasets/BGSsubset1true", "datasets/BGSsubset2true", "datasets/BGSsubset3true", "datasets/BGSsubset4true", 
+				"datasets/BGSsubset5true", "datasets/BGSsubset6true", "datasets/BGSsubset7true",
+				"datasets/BGSsubset8true", "datasets/BGSsubset9true", "datasets/BGSsubset10true"};
+				
+				//*/
+		
+	
+		for (int depth : depths) {
+			GraphList<DTGraph<String,String>> graphs = new GraphList<DTGraph<String,String>>(new ArrayList<DTGraph<String,String>>());
+			GraphList<DTGraph<String,String>> trees = new GraphList<DTGraph<String,String>>(new ArrayList<DTGraph<String,String>>());
+			
+			for (String subset : subsets) {
+				ClassificationDataSet ds = new SubsetDataSet(subset);
+				ds.create();
+				Set<Statement> st = RDFUtils.getStatements4Depth(ds.getRDFData().getDataset(), ds.getRDFData().getInstances(), depth, false);
+				st.removeAll(ds.getRDFData().getBlackList());
+				SingleDTGraph graph = RDFUtils.statements2Graph(st, RDFUtils.REGULAR_LITERALS, ds.getRDFData().getInstances(), true);
 
+				graphs.getGraphs().addAll(RDFUtils.getSubGraphs(graph.getGraph(), graph.getInstances(), depth).getGraphs());
+				trees.getGraphs().addAll(RDFUtils.getSubTrees(graph.getGraph(), graph.getInstances(), depth).getGraphs());
+			}
+			
+			System.out.println("Graph, depth: " + depth + ", " + computeGraphStatistics(graphs));		
+			System.out.println("Tree, depth: " + depth + ", " + computeGraphStatistics(trees));
+		}
 	}
 
 	private static String computeGraphStatistics(GraphList<DTGraph<String,String>> graphs) {
